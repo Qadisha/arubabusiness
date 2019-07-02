7194f3a13d2207ae51aa059ba5a294f0c631f225
Qadisha srls <35612215+Qadisha@users.noreply.github.com>
2019-07-03 00:09:57 +0300

    Update UpdateTTL.php

--- UpdateTTL.php
+++ UpdateTTL.php
@@ -1,2 +1,27 @@
 <?php
 
+function updateRecord($idRecord, $domain, $newrecord, $updateType){
+        
+	$token = Configuration::AcquireToken();   
+        
+        if($updateType == 'TTL'){
+            $resourcePath = '/api/domains/dns/ttl';
+            $data = array("Domain" => (string)$domain , "Ttl" => (string)$newrecord);
+        }      
+        $data_json = json_encode($data);
+        
+	$url= Configuration::GetUrl($resourcePath, $params=NULL);
+	$headr = Configuration::GetHeader($token, $qdput = $data_json);
+
+	$crl = curl_init();
+	curl_setopt($crl, CURLOPT_HTTPHEADER, $headr);
+	curl_setopt($crl, CURLOPT_URL, $url);        
+  curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($crl, CURLOPT_CUSTOMREQUEST, "PUT"); 
+  curl_setopt($crl, CURLOPT_POSTFIELDS,$data_json);
+
+	$rest = curl_exec($crl);     
+	curl_close($crl);
+
+	var_dump($rest); 
+}
